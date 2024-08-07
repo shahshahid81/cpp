05a7a9304c35b9cd111352780e3751b0496e05c3
Shahid Shah <shahshahid2123@gmail.com>
2024-08-07 07:07:17 +0530

    feat: variable init

---
+++ 003-Variables-And-Constants/area.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    cout << "Width: " << endl;
+    // C-like initialization
+    // int width = 0;
+    // Constructor initialization
+    // int width(0);
+    // C++11 list initialization sytanx, recommended approach
+    int width{0};
+    cin >> width;
+
+    cout << "Height: " << endl;
+    int height{0};
+    cin >> height;
+
+    cout << "Area: " << width * height << endl;
+
+    return 0;
+}
