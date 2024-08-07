b048383c3f1a82cefac60a16c56890d37a938836
Shahid Shah <shahshahid2123@gmail.com>
2024-08-07 06:28:50 +0530

    feat: added basic io program

--- 001-First-Program/main.cpp
+++ 001-First-Program/main.cpp
@@ -1,7 +1,11 @@
+// https://youtu.be/6KNdGnUiRBM?si=mt7vWTsF8bKfoG-K
+// https://en.cppreference.com/w/cpp/preprocessor
 #include <iostream>
 
 int main()
 {
+    // https://stackoverflow.com/questions/1422729/how-does-an-uninitiliazed-variable-get-a-garbage-value
+    // Variable is uninitialized and will have a garbage value.
     int favorite_number;
     std::cout << "Enter a number between 1 and 100:" << std::endl;
     std::cin >> favorite_number;
